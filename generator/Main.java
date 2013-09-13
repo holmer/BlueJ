20021d94ace84538c4e1342a04a42a8c9e703bdc
Holmer <holmer@chalmers.se>
2013-09-13 09:50:22 +0200

    Added readme to Main

--- generator/Main.java
+++ generator/Main.java
@@ -1,10 +1,18 @@
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Test the generator class.
+ * @author  Uno Holmer
+ * @version 2013-09-13
+ */
 public class Main {
     public static void main(String[] arg) {
        
-        List<Integer> numbers = CollectionsPlusPlus.generate(new IntGenerator(0,1),10000);
+        List<Integer> numbers = 
+            CollectionsPlusPlus.generate(
+                new IntGenerator(0,1),
+                10000);
           
         for ( int i = 1; i <= 1000; i++ ) {
             Collections.shuffle(numbers);
