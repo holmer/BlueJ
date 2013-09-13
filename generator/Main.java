9ef6b3bb4689c92d647b429e85cdd2b2ba960255
holmer <holmer@chalmers.se>
2013-09-13 11:14:05 +0200

    Resolved conflict in Main (?)

--- generator/Main.java
+++ generator/Main.java
@@ -4,7 +4,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] arg) {
        
-        List<Integer> numbers = CollectionsPlusPlus.generate(new IntGenerator(0,1),10000);
+        List<Integer> numbers = 
+            CollectionsPlusPlus.generate(
+                new IntGenerator(0,1),
+                10000);
           
         for ( int i = 1; i <= 1000; i++ ) {
             Collections.shuffle(numbers);
