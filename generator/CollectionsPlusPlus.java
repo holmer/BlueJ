ccdf027b4600ce1eeaa35452dc409923b5ca33e1
holmer <holmer@chalmers.se>
2013-10-09 14:17:46 +0200

    Changed to LinkedList in filter alg

--- generator/CollectionsPlusPlus.java
+++ generator/CollectionsPlusPlus.java
@@ -1,10 +1,10 @@
 import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class CollectionsPlusPlus {
 
     public static <T> List<T> generate(Generator<T> generator, int n) {
-        ArrayList<T> l = new ArrayList<T>(n);
+        List<T> l = new LinkedList<T>();
         
         for ( int i = 0; i < n; i++ ) 
             l.add(generator.next());
