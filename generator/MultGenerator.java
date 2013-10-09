03f94c7e69b687a45af378ed4c79cb3b80aded45
Holmer <holmer@chalmers.se>
2013-10-09 23:03:29 +0200

    Add a new progressive generator

---
+++ generator/MultGenerator.java
@@ -0,0 +1,16 @@
+public class MultGenerator implements Generator<Integer> {
+	private int currentValue;
+	private int increment;
+	
+	public MultGenerator( int startValue, int increment ) {
+		currentValue = startValue;
+		this.increment = increment;
+	}
+
+	public Integer next() {
+	    int v = currentValue;
+	    increment++;
+	    currentValue += increment; 
+	    return v;
+	}
+}
