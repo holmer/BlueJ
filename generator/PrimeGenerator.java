024fcd6c518a0dc0e3347ecd59b9e76b04fa55ed
holmer <holmer@chalmers.se>
2013-10-18 15:16:49 +0200

    Fix issue in primegen

--- generator/PrimeGenerator.java
+++ generator/PrimeGenerator.java
@@ -2,11 +2,20 @@ public class PrimeGenerator implements Generator<Integer> {
 	private int currentPrime = 1;
 
 	public Integer next() {
-	    return currentPrime;
+	    int returnPrime = currentPrime;
+	    if ( currentPrime == 1 )
+	        currentPrime = 2;
+	    else if ( currentPrime == 2 )
+	        currentPrime = 3;
+	    else
+	        do {
+	            currentPrime += 2;
+            } while ( ! isPrime(currentPrime) );
+	    return returnPrime;
 	}
 	
 	private boolean isPrime(int n) {
-	    for ( int f = 2; f < (int)Math.sqrt(n); f += 2)
+	    for ( int f = 2; f <= (int)Math.sqrt(n); f++ )
 	        if ( n % f == 0 )
 	            return false;
 	    return true;
