dad1bb849e0f4ff141d1a0ed26f20808f7e61b7a
holmer <holmer@chalmers.se>
2013-10-22 16:31:15 +0200

    Add backwards generator

---
+++ generator/BackwardsIntGenerator.java
@@ -0,0 +1,16 @@
+
+public class BackwardsIntGenerator {
+	private int currentValue;
+	private int decrement;
+	
+	public BackwardsIntGenerator( int startValue, int decrement ) {
+		currentValue = startValue;
+		this.decrement = decrement;
+	}
+
+	public Integer next() {
+	    int v = currentValue;
+	    currentValue -= decrement; 
+	    return v;
+	}
+}
